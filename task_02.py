0233319d0078aa3da3b5a96939f235d03db270b1
Dmitriy <pd2018@yandex.ru>
2023-05-22 12:15:11 +0300

    Task_02 modified.

--- task_02.py
+++ task_02.py
@@ -1,8 +1,14 @@
-print("Сумма цифр трёхзначного числа.\n")
-inNum = int(input("Введите 3-х значное число: "))
+# Caption.
+print("Сумма цифр 3-значного числа.\n")
+
+# Data input.
+inNum = int(input("Введите 3-значное число: "))
+
 n = inNum
 sum = 0
 while n > 0:
     sum += n % 10
     n //= 10
+
+# Rezult output.
 print(f"{inNum} -> {sum} ({inNum//100} + {inNum//10%10} + {inNum%10})")
