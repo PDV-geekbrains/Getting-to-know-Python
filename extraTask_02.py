183cdb5a4672b634a55e0ce9d93bc4211f960b41
Dmitriy <pd2018@yandex.ru>
2023-05-22 15:21:25 +0300

    Added extraTask_02.py

---
+++ extraTask_02.py
@@ -0,0 +1,16 @@
+# Caption.
+print("Сумма элементов списка, стоящих на нечётных позициях.\n")
+
+# Data input.
+inputData = input("Введите ряд целых чисел через пробел: ").split()
+
+inputArray =[]
+for x in inputData:
+    inputArray.append(int(x))
+
+sum = 0
+for x in range(1, len(inputArray), 2):
+    sum += inputArray[x]
+
+# Rezult output.
+print(f'{inputArray} -> {sum}\n')
