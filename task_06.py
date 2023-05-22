e327f258d69f2a639ec772d982af84189f44bbfa
Dmitriy <pd2018@yandex.ru>
2023-05-22 12:21:23 +0300

    Task_06 modified

--- task_06.py
+++ task_06.py
@@ -1,6 +1,7 @@
 # Task caption.
 print("Счастливый билетик.\n")
 
+# Data input.
 ticketNumber = int(input("Введите 6-значный номер проездного билета: "))
 
 def sum (number):
@@ -11,6 +12,9 @@ def sum (number):
     return s
 
 if(sum(ticketNumber // 1000) == sum(ticketNumber % 1000)):
+# Result output.
     print("yes")
 else: 
     print("no")
+
+
