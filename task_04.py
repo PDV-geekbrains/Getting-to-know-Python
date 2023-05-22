c7d187e8fd67e9b67c7700f41e9e1c2380295018
Dmitriy <pd2018@yandex.ru>
2023-05-22 12:17:07 +0300

    Task_04 modified.

--- task_04.py
+++ task_04.py
@@ -1,9 +1,13 @@
 # Task caption.
 print("Детские игрушки.\n")
 
+# Data input.
 toysNumber = int(input("Введите общее количество игрушек: "))
+
 x = int(toysNumber / 6)
 k = 4 * x
 s = x
 p = x
+
+# Rezult output 
 print(f"{toysNumber} -> {s} {k} {p}")
