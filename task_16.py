7f422d0e5fd97631cb098f42458b5c5fecfac1dd
Dmitriy <pd2018@yandex.ru>
2023-05-28 12:22:36 +0300

    Added task_16

---
+++ task_16.py
@@ -0,0 +1,19 @@
+import random
+
+# Caption.
+print("Количество вхождений числа K в массив A[1..N].")
+
+# Data input.
+N = int(input("Введите число элементов в массиве (N): "))
+K = int(input('Введите число для поиска (K): '))
+
+n = 0
+arr = []
+for i in range(N):
+    arr.append(random.randint(1, N))
+for i in range(0, len(arr)):
+    if K == arr[i]:
+        n += 1
+
+print(arr)
+print(n)
